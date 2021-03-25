237287032a2df11df5d23c7deba04bcddad8b28b
Johan Peláez <japaneva@gmail.com>
2021-03-25 14:48:25 -0500

    adding changes and removing JQuery

--- js/app.js
+++ js/app.js
@@ -1,44 +1,24 @@
-$(document).ready(function(){
-    M.AutoInit();
-    const showReplies = $('#show-replies')
-    const theme = $('#theme')
-    const dnt = $('#dnt')
-    const chrome = $('.chrome')
-    const width = $('#width')
-    const height = $('#height')
-    const tweetLimit = $('#tweet-limit')
-    const getLink = $('#get-link')
-
-    $('#generar-link').click(() => {
-        let chromeValues = '';
-        chrome.each((i, v) => {
-            if ($(v).is(':checked')) chromeValues += $(v).val() + '-'
-        })
-        const link = `http://localhost/bear/WidgetsTres/Twitter/diseno/twitterTest.html?show-replies=${showReplies.is(":checked")}&theme=${theme.is(":checked")? 'dark' : 'light'}&chrome=${chromeValues.slice(0,-1)}&width=${width.val()}&height=${height.val()}&tweet-limit=${tweetLimit.val()}`
-        getLink.val(link);
-    })
-
-});
-/*
 document.addEventListener('DOMContentLoaded', function() {
     M.AutoInit();
-    const showReplies = document.getElementById('#show-replies')
-    const theme = document.getElementById('#theme')
-    const dnt = document.getElementById('#dnt')
-    const chrome = document.getElementByClassName('.chrome')
-    const width = document.getElementById('#width')
-    const height = document.getElementById('#height')
-    const tweetLimit = document.getElementById('#tweet-limit')
-    const getLink = document.getElementById('#get-link')
+    const url = "http://localhost/multimediacorp/twitterWidget.php"
+    const showReplies = document.getElementById('show-replies')
+    const theme = document.getElementById('theme')
+    const dnt = document.getElementById('dnt')
+    const chrome = document.getElementsByClassName('chrome')
+    const width = document.getElementById('width')
+    const height = document.getElementById('height')
+    const tweetLimit = document.getElementById('tweet-limit')
+    const getLink = document.getElementById('get-link')
+    const generarLink = document.getElementById('generar-link')
 
-    $('#generar-link').click(() => {
+    generarLink.addEventListener('click', () => {
         let chromeValues = '';
-        chrome.each((i, v) => {
-            if ($(v).is(':checked')) chromeValues += $(v).val() + '-'
-        })
-        const link = `http://localhost/bear/WidgetsTres/Twitter/diseno/twitterTest.html?show-replies=${showReplies.is(":checked")}&theme=${theme.is(":checked")? 'dark' : 'light'}&chrome=${chromeValues.slice(0,-1)}&width=${width.val()}&height=${height.val()}&tweet-limit=${tweetLimit.val()}`
-        getLink.val(link);
+        for (const v of chrome) {
+            if (v.checked) chromeValues += v.value + '-'
+        }
+        const link = `${url}?show-replies=${showReplies.checked}&dnt=${dnt.checked}&theme=${theme.checked? 'dark' : 'light'}&chrome=${chromeValues.slice(0,-1)}&width=${width.value||0}&height=${height.value||0}&tweet-limit=${tweetLimit.value}`
+        getLink.value = link;
     })
 });
- */
+
 
